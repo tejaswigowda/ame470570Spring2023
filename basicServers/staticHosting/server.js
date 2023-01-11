14c7f4e7996f0156f91c2b46e9d7781f655c4c43
Tejaswi Gowda <tejaswi@asu.edu>
2023-01-11 11:28:26 -0700

    ac

--- basicServers/staticHosting/server.js
+++ basicServers/staticHosting/server.js
@@ -1,13 +1,13 @@
 var express = require("express");
-var app = express();
+var server = express();
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 1234;
 
-app.get("/", function (req, res) {
+server.get("/", function (req, res) {
     res.redirect("index.html")
 });
 
-app.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public'));
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+server.listen(port);
