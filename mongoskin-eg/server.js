d65328d260ad2a8ee5fd8a8aa0a321449a246c49
Tejaswi Gowda <tejaswi@asu.edu>
2023-02-08 11:31:09 -0700

    in class

--- mongoskin-eg/server.js
+++ mongoskin-eg/server.js
@@ -18,11 +18,11 @@ var port = 1234;
 server.get("/add", function (req, res) {
   console.log(req.query);
   db.collection("data").insert(req.query, function(err, result) {
-  });
-
     var a = parseFloat(req.query.a);
     var b = parseFloat(req.query.b);
     res.send((a+b).toString()); // send response body
+  });
+
 
 });
 
