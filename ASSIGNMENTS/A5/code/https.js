b530c702ef4861a62a4f474246cb9b74ad6ab005
Tejaswi Gowda <tejaswi@asu.edu>
2023-03-29 11:32:29 -0700

    ac

--- ASSIGNMENTS/A5/code/https.js
+++ ASSIGNMENTS/A5/code/https.js
@@ -3,9 +3,9 @@ require('greenlock-express').create({
   version: 'draft-11'
 , server: 'https://acme-v02.api.letsencrypt.org/directory'
 //, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
-, email: ''                                     // CHANGE THIS
+, email: 'tejaswi@asu.edu'                                     // CHANGE THIS
 , agreeTos: true
-, approveDomains: [ 'example.com', 'www.example.com' ]              // CHANGE THIS
+, approveDomains: [ 'tlingego.httpsexample.xyz', 'www.tlingego.httpsexample.xyz' ]              // CHANGE THIS
 , store: require('greenlock-store-fs')
 , configDir: homeDir
 //, app: require('express')().use('/', function (req, res) {
