8a1f003295f13031bd399a3f18e62fe4924f9680
Tejaswi Gowda <tejaswi@asu.edu>
2023-04-03 10:42:15 -0700

    in class

--- imageApp/server.js
+++ imageApp/server.js
@@ -1,61 +1,68 @@
+var url = require("url"),
+	querystring = require("querystring");
+var passport = require('passport');
 var fs = require('fs');
-//var AWS = require('aws-sdk');
-//AWS.config.loadFromPath('./credentials.json');
-//var s3 = new AWS.S3();
+	var dbURL = 'mongodb://3.101.54.125:27017/test';
 
-var express = require("express");
-var app = express();
-var bodyParser = require('body-parser');
+    var args = process.argv.slice(2);
+    if (args[0] == "dev") {
+        dbURL = 'mongodb://localhost:27017/test';
+    }     
 
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8080;
-app.use(methodOverride());
-//app.use(bodyParser());
-app.use(require('connect').bodyParser());
-
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(express.static(__dirname + '/public'));
-app.use(errorHandler());
-
-app.get("/", function (req, res) {
-      res.redirect("/index.html");
-});
-
-/*
-app.post('/uploadFile', function(req, res){
-    var intname = req.body.fileInput;
-    var filename = req.files.input.name;
-    var fileType =  req.files.input.type;
-    var tmpPath = req.files.input.path;
-    var s3Path = '/' + intname;
     
-    fs.readFile(tmpPath, function (err, data) {
-        var params = {
-            Bucket:'bucket470570',
-            ACL:'public-read',
-            Key:intname,
-            Body: data,
-            ServerSideEncryption : 'AES256'
-        };
-        s3.putObject(params, function(err, data) {
-            res.end("success");
-            console.log(err);
-        });
-    });
-});
-*/
+var path = require('path'),
+  express = require('express'),
+  db = require('mongoskin').db(dbURL);
 
-console.log("Simple static server listening at http://" + hostname + ":" + port);
-//app.listen(port);
-// DO NOT DO app.listen() unless we're testing this directly
-if (require.main === module) { app.listen(8080); }
-// Instead do export the app:
-else{ module.exports = app; }
+
+var mongoose = require('mongoose');
+mongoose.connect(dbURL); // connect to our database
+
+var app = express();
+var secret = 'test' + new Date().getTime().toString()
+
+var session = require('express-session');
+app.use(require("cookie-parser")(secret));
+var MongoStore = require('connect-mongo')(session);
+app.use(session( {store: new MongoStore({
+   url: dbURL,
+   secret: secret
+})}));
+app.use(passport.initialize());
+app.use(passport.session());
+var flash = require('express-flash');
+app.use( flash() );
+
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+app.use(methodOverride());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended:false
+}));
+require('./passport/config/passport')(passport); // pass passport for configuration
+require('./passport/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+
+app.get("/addImage", isLoggedIn, function(req,res){
+
+})
+
+app.get("/getImages", function(req,res){
+
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(8080);
+
+console.log("server running at http://localhost:8080")
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.send('noauth');
+}
+
